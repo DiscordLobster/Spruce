49ae1e095b0fff3e17402792582af178d355cb76
Capricorn <jmgross1997@gmail.com>
2022-12-27 17:58:22 -0500

    Added dynamic prefix command execution

--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -3,10 +3,9 @@ require('dotenv').config();
 module.exports = {
   name: 'messageCreate',
   async execute(message, client) {
-    const { devCommands } = client;
-    const { author } = message;
+    const { devCommands, prefixCmds } = client;
 
-    if (author.id === process.env.DEV_ID) {
+    /** if (message.author.id === process.env.DEV_ID) {
       const devCommand = devCommands.get(message.content);
       if (!devCommand) return;
 
@@ -15,6 +14,25 @@ module.exports = {
       }
       catch (err) {
         console.error(err);
+      }
+    } **/
+    
+    if (message.toString().startsWith('s!')) {
+
+      const args = message.toString().slice(2).trim().split(/ +/);
+	    const commandName = args.shift().toLowerCase();
+
+      if (!prefixCmds.has(commandName)) return;
+
+      const command = prefixCmds.get(commandName);
+
+      try {
+
+        command.execute(message, args, client);
+      }
+      catch (error) {
+        console.error(error);
+        message.reply('There was an error executing this command!');
       }
     }
   },
