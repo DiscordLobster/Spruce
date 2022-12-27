aa0fa73664e11c910673f86138b6c6af8cccdd3c
Capricorn <jmgross1997@gmail.com>
2022-12-27 17:56:46 -0500

    Added new settings to change

--- src/models/settingsModel.js
+++ src/models/settingsModel.js
@@ -4,11 +4,24 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       primaryKey: true
     },
+    prefix: {
+      type: DataTypes.STRING,
+      defaultValue: 's!',
+      allowNull: false,
+    },
     mod_logs: DataTypes.STRING,
     member_logs: DataTypes.STRING,
     channel_logs: DataTypes.STRING,
     message_logs: DataTypes.STRING,
     update_channel: DataTypes.STRING,
     ticket_logs: DataTypes.STRING,
+    ticket_channel: DataTypes.STRING,
+    suggest_channel: DataTypes.STRING,
+    helper_icon: DataTypes.STRING,
+    mod_icon: DataTypes.STRING,
+    admin_icon: DataTypes.STRING,
+    manager_icon: DataTypes.STRING,
+    owner_icon: DataTypes.STRING,
+    developer_icon: DataTypes.STRING,
   });
 }
