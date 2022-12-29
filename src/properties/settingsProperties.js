2ae75b531f8d097d6e493fb79fa4b408e56b0190
Capricorn <jmgross1997@gmail.com>
2022-12-29 09:29:18 -0500

    Added comments to detail what each property does

--- src/properties/settingsProperties.js
+++ src/properties/settingsProperties.js
@@ -4,6 +4,16 @@ const { createWriteStream } = require('fs');
 const logger = createWriteStream('./src/logs/settings.log', { flags: 'a' });
 
 module.exports = (collection) => {
+
+  /**
+   * botSettings.add(guildId);
+   * 
+   * Adds a new settings entry to the database and saves it to the bot's local cache. Similarily, the bot also logs the output
+   * to a log file in the logs folder. If settings already exist for this id it returns false.
+   * 
+   * guildId - a valid Discord Guild ID
+   */
+
   Reflect.defineProperty(collection, 'add', {
     value: async (guildId) => {
       const dateFormat = dayjs(Date.now()).format('YYYY-MM-DD hh:mm:ss');
@@ -19,6 +29,16 @@ module.exports = (collection) => {
       }
     }
   });
+
+  /**
+   * botSettings.remove(guildId);
+   * 
+   * Removes a settings entry for a guild based on it's ID by searching for a query in the database. If the query exists it
+   * deletes the entry and returns the settings before they were deleted. If no entry was found it returns false. This also
+   * removes the data from the bot's local cache too.
+   * 
+   * guildId - a valid Discord Guild ID
+   */
 
   Reflect.defineProperty(collection, 'remove', {
     value: async (guildId) => {
@@ -36,6 +56,15 @@ module.exports = (collection) => {
     }
   });
 
+  /**
+   * botSettings.fetch(guildId);
+   * 
+   * Fetches the settings data for a guild's bot settings by it's server ID and returns the data. It tries to pull from the local
+   * cache first and then queries the database if no result was found. If no settings can be found it returns false.
+   * 
+   * guildId - a valid Discord Guild ID
+   */
+
   Reflect.defineProperty(collection, 'fetch', {
     value: async (guildId) => {
       const settings = collection.get(guildId) || await Settings.findOne({ where: { guild_id: guildId } });
@@ -43,6 +72,15 @@ module.exports = (collection) => {
       else return settings;
     }
   });
+
+  /**
+   * botSettings._sync(guildId);
+   * 
+   * (Only used in this file itself not required outside)
+   * Syncs the queried data from the database to the local cache or 'collection'
+   * 
+   * guildId - A valid Discord Guild ID
+   */
 
   Reflect.defineProperty(collection, '_sync', {
     value: async (guildId) => {
