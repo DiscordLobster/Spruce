14a7fb08652295949888f4adee32337704d2ea47
Capricorn <jmgross1997@gmail.com>
2023-01-03 07:25:45 -0500

    Added dev cache command

---
+++ src/legacyCommands/Developer/cache.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'cache',
+  description: 'Developer Only',
+  category: 'Developer',
+  devOnly: true,
+  async execute(message, args, client) {
+    const { modWarnings, modMutes, modBans, commands, prefixCmds, buttons, menus, modals } = client;
+  },
+};
