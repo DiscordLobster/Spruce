bf19753a900186c6952602ff772c224320e8fb81
Capricorn <jmgross1997@gmail.com>
2023-01-03 07:24:26 -0500

    Updated ready event

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,7 +1,11 @@
+const { ActivityType } = require("discord.js");
+
 module.exports = {
   name: 'ready',
   once: true,
   async execute(client) {
     console.log(`Successfully logged in as ${client.user.tag}!`);
+
+    client.user.setPresence({ activities: [{ name: 'with lots of bugs!', type: ActivityType.Playing }], status: 'dnd' });
   },
 };
